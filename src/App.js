71e41214634640a0b548822a0eead41f9f31879b
Emily Siu <emsiu@umich.edu>
2019-06-07 21:22:23 -0400

    Display list of items that are entered in a form

--- src/App.js
+++ src/App.js
@@ -2,25 +2,73 @@ import React from 'react';
 import logo from './logo.svg';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class StardewValleyApp extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      items: [],
+      text: ''
+    };
+    this.handleChange = this.handleChange.bind(this);
+    this.handleSubmit = this.handleSubmit.bind(this);
+  }
+
+  render() {
+    return (
+      <div className="App">
+        <header className="App-header">
+          <img src={logo} className="App-logo" alt="logo" />
+          <ItemList items={this.state.items} />
+          <form onSubmit={this.handleSubmit}>
+            <label htmlFor="new-item">Add an item</label>
+            <br></br>
+            <input
+              id="new-item"
+              onChange={this.handleChange}
+              value={this.state.text}
+            />
+            <br></br>
+            <button>
+              Add item #{this.state.items.length + 1}
+            </button>
+          </form>
+        </header>
+      </div>
+    );
+  }
+  
+  handleChange(e) {
+    this.setState({
+      text: e.target.value
+    });
+  }
+
+  handleSubmit(e) {
+    e.preventDefault();
+    if (!this.state.text.length) {
+      return;
+    }
+    const newItem = {
+      text: this.state.text,
+      id: Date.now()
+    };
+    this.setState(state => ({
+      items: state.items.concat(newItem),
+      text: ''
+    }));
+  }
 }
 
-export default App;
+class ItemList extends React.Component {
+  render() {
+    return (
+      <ul>
+        {this.props.items.map(item => (
+          <li key={item.id}>{item.text}</li>
+        ))}
+      </ul>
+    );
+  }
+}
+
+export default StardewValleyApp;
